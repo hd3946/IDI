188b8ec0ff64ae506ca550d8b8f5ed5a32983555
임형대 <imhyeongdae@imhyeongdaeui-MacBook-Pro.local>
2018-07-24 23:25:49 +0900

    url

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.urls import include, path
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
