f239b9c6f5f3d434b9b299aa60e2286727c1d99b
임형대 <imhyeongdae@imhyeongdaeui-MacBook-Pro.local>
2018-07-25 00:04:04 +0900

    host

--- config/settings/production.py
+++ config/settings/production.py
@@ -43,7 +43,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['example.com'])
+ALLOWED_HOSTS = ["*"]
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ['gunicorn', ]
